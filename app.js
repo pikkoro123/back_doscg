23d4bb630463eeef50fe575bdf2afa2f25bf7d7b
Pallop <jibbiw123456@gmail.com>
2020-09-03 23:49:00 +0700

    fixed bug

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/images", express.static(path.join("images")));
-
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -27,6 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/images", express.static(path.join("images")));
 app.use("/api", routes);
 app.use("", routesLine);
 // app.use("/api/user", userRoutes);
